020cfe10e2aa4715dc309ab5eb9ad97c2bd7a379
jackykwe <40329372+jackykwe@users.noreply.github.com>
2022-07-31 18:10:07 +0800

    Added dev database

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,12 +5,15 @@ mod types;
 
 use actix_web::{web, App, HttpResponse, HttpServer};
 use dotenv::dotenv;
-use log::info;
+use log::{info, warn};
 use r2d2_sqlite::SqliteConnectionManager;
 use std::io::{Error as IOError, ErrorKind};
 
 use crate::app_env_config::AppEnvConfig as AEC;
 use crate::db::types::Pool;
+
+const DATABASE_NAME_PROD: &str = "utilities.db";
+const DATABASE_NAME_DEV: &str = "utilities-dev.db";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -18,9 +21,16 @@ async fn main() -> std::io::Result<()> {
     let aec = AEC::default();
 
     env_logger::init(); // must come after dotenv().ok().
+    if aec.dev_mode {
+        warn!("You are in DEV_MODE. Do NOT use this in production: certain features are for development only.")
+    }
 
     // Connect to SQLite DB and initialise it if not already done
-    let manager = SqliteConnectionManager::file("utilities.db");
+    let manager = SqliteConnectionManager::file(if aec.dev_mode {
+        DATABASE_NAME_DEV
+    } else {
+        DATABASE_NAME_PROD
+    });
     let pool = Pool::new(manager).unwrap();
     db::init::initialise_database(&pool)
         .await
